d6f264b22fbf3b0f0cc8b2265a8334be467db183
eraj01788 <muntasirhosain@gmail.com>
2021-10-15 10:52:53 +0600

    Login form

--- e-hut/src/App.js
+++ e-hut/src/App.js
@@ -1,12 +1,13 @@
 import Products from "./Product/Products";
 import SignUp from "./SignUp/SignUp";
+import Login from "./Login/Login";
 
 function App() {
-	return (
-		<div className="App">
-			<SignUp />
-		</div>
-	);
+  return (
+    <div className="App">
+      <Login />
+    </div>
+  );
 }
 
 export default App;
