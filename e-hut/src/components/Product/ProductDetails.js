e1fb25124790882b940d7117b05d60b169ca05b0
eraj01788 <muntasirhosain@gmail.com>
2021-11-20 18:41:42 +0600

    dad

--- e-hut/src/components/Product/ProductDetails.js
+++ e-hut/src/components/Product/ProductDetails.js
@@ -1,15 +1,23 @@
-import axios from 'axios';
-import React, { useEffect, useState } from 'react';
-import { Button, Card, Col, Image, ListGroup, ListGroupItem, Row } from 'react-bootstrap';
-import { Link } from 'react-router-dom';
-import Pran from '../../images/pranup.jpg';
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import {
+  Button,
+  Card,
+  Col,
+  Image,
+  ListGroup,
+  ListGroupItem,
+  Row,
+} from "react-bootstrap";
+import { Link } from "react-router-dom";
+import Pran from "../../images/pranup.jpg";
 
-const ProductDetails = ({match}) => {
-    const [product, setProduct] = useState({})
+const ProductDetails = ({ match }) => {
+  const [product, setProduct] = useState({});
 
-    useEffect(() =>{
-        let id = match.params.id
-        axios
+  useEffect(() => {
+    let id = match.params.id;
+    axios
       .get(`https://localhost:44390/api/products/${id}`)
       .then((response) => {
         setProduct(response.data);
@@ -17,64 +25,57 @@ const ProductDetails = ({match}) => {
       .catch((err) => {
         console.log(err);
       });
-    })
-    return (
-        <React.Fragment>
-            <Link className='btn btn-success m-3' to='/'>
-            Go Back
-        </Link>
-       
-            <Row>
-                <Col md={3}> </Col>
-                <Col md={4} className='mb-5'>
-                    <Image  src={Pran} alt='Pran Up' height='400' width='450'/>
-                    {/* fluid will make the image to be inside the container and stop it from going outside of it. */}
-                </Col>
-                <Col md={3} className='my-5'>
-                    <Card>
-                        <ListGroupItem variant='flush'>
-                            {/*flush variant takes away the spacing */}
-                            <ListGroupItem>
-                            <h3>{product.Name}</h3>
-                            </ListGroupItem>
-                            <ListGroupItem>
-                                <Row>
-                                    <Col>
-                                        Price:
-                                    </Col>
-                                    <Col>
-                                        <strong>${product.Price}</strong>
-                                    </Col>
-                                </Row>
-                            </ListGroupItem>
-                            <ListGroupItem>
-                            Description: {product.Details}
-                            </ListGroupItem>
-                            <ListGroupItem>
-                                <Row>
-                                    <Col>
-                                        Status:
-                                    </Col>
-                                    <Col>
-                                        {product.Status ? 'In Stock' : 'Out of Stock'}
-                                    </Col>
-                                </Row>
-                            </ListGroupItem>
-                            
-                            <ListGroupItem>
-                                <Button className='btn-block btn-success'
-                                    type='button' disabled={product.Status === false}
-                                    >Add To Cart</Button>
-                            </ListGroupItem>
+  });
+  return (
+    <React.Fragment>
+      <Link className="btn btn-success m-3" to="/">
+        Go Back
+      </Link>
 
-                        </ListGroupItem>
-                    </Card>
-                </Col>
-            </Row>
-        
-        </React.Fragment>
-    )
+      <Row>
+        <Col md={3}> </Col>
+        <Col md={4} className="mb-5">
+          <Image src={Pran} alt="Pran Up" height="400" width="450" />
+          {/* fluid will make the image to be inside the container and stop it from going outside of it. */}
+        </Col>
+        <Col md={3} className="my-5">
+          <Card>
+            <ListGroupItem variant="flush">
+              {/*flush variant takes away the spacing */}
+              <ListGroupItem>
+                <h3>{product.name}</h3>
+              </ListGroupItem>
+              <ListGroupItem>
+                <Row>
+                  <Col>Price:</Col>
+                  <Col>
+                    <strong>${product.price}</strong>
+                  </Col>
+                </Row>
+              </ListGroupItem>
+              <ListGroupItem>Description: {product.details}</ListGroupItem>
+              <ListGroupItem>
+                <Row>
+                  <Col>Status:</Col>
+                  <Col>{product.status ? "In Stock" : "Out of Stock"}</Col>
+                </Row>
+              </ListGroupItem>
 
-}
+              <ListGroupItem>
+                <Button
+                  className="btn-block btn-success"
+                  type="button"
+                  disabled={product.Status === false}
+                >
+                  Add To Cart
+                </Button>
+              </ListGroupItem>
+            </ListGroupItem>
+          </Card>
+        </Col>
+      </Row>
+    </React.Fragment>
+  );
+};
 
-export default ProductDetails
+export default ProductDetails;
