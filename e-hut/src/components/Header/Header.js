05c515f48548186ecbee6debd50cae208cfaf569
eraj01788 <muntasirhosain@gmail.com>
2021-11-26 14:46:17 +0600

    searchPro

--- e-hut/src/components/Header/Header.js
+++ e-hut/src/components/Header/Header.js
@@ -1,33 +1,72 @@
-import React from 'react'
-import { NavLink } from 'react-router-dom'
+import React, { useState } from "react";
+import { NavLink } from "react-router-dom";
+import Product from "../Product/Product";
 
 const Header = () => {
-    return (
-        <React.Fragment>
-           <nav className="navbar navbar-light " style={{ backgroundColor: "#21D192" }}>
-            <div className="container-fluid ">
-               <NavLink to='/' style={{ textDecoration: 'none' }}> <span className="navbar-brand mb-0 h1 fs-1 text-white">E-HUT</span> </NavLink>
-           
-            <form className="d-flex">
-            <input className="form-control me-2 " type="search" placeholder="Search" aria-label="Search" />
-            <button className="btn btn-outline-success text-white" type="submit">Search</button>
-            </form>
-            <div >
-                <span className="me-5 text-white"><i className="fas fa-shopping-cart"></i> Cart </span>
-                <span className="me-5 text-white"><i className="fas fa-user"></i> User </span>
-                <span>
-                <NavLink to='/signup' style={{ textDecoration: 'none', color:'white' }}>Sign Up</NavLink>
-                <span style={{ color: 'white' }}> or </span>
-                <NavLink to='/login' style={{ textDecoration: 'none', color:'white' }}>Login</NavLink>
-                </span>
-            </div>
-           
-            </div>
-            
-           </nav>
-            
-        </React.Fragment>
-    )
-}
+  const [searchProduct, setSearchProduct] = useState("");
 
-export default Header
+  const SearchProduct = (e) => {
+    e.preventDefault();
+    console.log(searchProduct);
+  };
+
+  return (
+    <React.Fragment>
+      <nav
+        className="navbar navbar-light "
+        style={{ backgroundColor: "#21D192" }}
+      >
+        <div className="container-fluid ">
+          <NavLink to="/" style={{ textDecoration: "none" }}>
+            {" "}
+            <span className="navbar-brand mb-0 h1 fs-1 text-white">
+              E-HUT
+            </span>{" "}
+          </NavLink>
+
+          <form className="d-flex">
+            <input
+              className="form-control me-2 "
+              type="search"
+              placeholder="Search"
+              aria-label="Search"
+              onChange={(e) => setSearchProduct(e.target.value)}
+            />
+            <button
+              className="btn btn-outline-success text-white"
+              type="submit"
+              onClick={SearchProduct}
+            >
+              Search
+            </button>
+          </form>
+          <div>
+            <span className="me-5 text-white">
+              <i className="fas fa-shopping-cart"></i> Cart{" "}
+            </span>
+            <span className="me-5 text-white">
+              <i className="fas fa-user"></i> User{" "}
+            </span>
+            <span>
+              <NavLink
+                to="/signup"
+                style={{ textDecoration: "none", color: "white" }}
+              >
+                Sign Up
+              </NavLink>
+              <span style={{ color: "white" }}> or </span>
+              <NavLink
+                to="/login"
+                style={{ textDecoration: "none", color: "white" }}
+              >
+                Login
+              </NavLink>
+            </span>
+          </div>
+        </div>
+      </nav>
+    </React.Fragment>
+  );
+};
+
+export default Header;
