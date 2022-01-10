a9241a00e31f578a59e314eec18d28bbd408a364
eraj01788 <muntasirhosain@gmail.com>
2022-01-10 10:56:25 +0600

    OrderList

--- e-hut/src/components/Shops/Dashboard.js
+++ e-hut/src/components/Shops/Dashboard.js
@@ -6,6 +6,7 @@ import "./Dashboard.css";
 const ShopDashboard = (props) => {
   var data = JSON.parse(localStorage.getItem("user"));
   const [countPendingOrder, setCountPendingOrder] = useState(0);
+  const [pendingProduct, setPendingProduct] = useState([]);
   useEffect(() => {
     axios
       .get(
@@ -14,11 +15,17 @@ const ShopDashboard = (props) => {
       )
       .then((response) => {
         setCountPendingOrder(response.data.length);
+        setPendingProduct(response.data);
+        console.log(response.data[0].Product.Name);
       })
       .catch((err) => {
         console.log(err);
       });
   }, []);
+
+  const OnClickPendingOrder = (e) => {
+    document.querySelector(".main_content").style.display = "block";
+  };
 
   return (
     <React.Fragment>
@@ -30,11 +37,43 @@ const ShopDashboard = (props) => {
         <div className="shadow-box-example z-depth-5">
           <p id="tOrderText">Pending Order</p>
           <p id="tOrderCount">{countPendingOrder}</p>
+          <button
+            id="penBtn"
+            type="button"
+            class="btn btn-outline-primary "
+            onClick={OnClickPendingOrder}
+          >
+            Details
+          </button>
         </div>
         <div className="shadow-box-example z-depth-5">
           <p id="tOrderText">Total Sold</p>
           <p id="tOrderCount">2</p>
         </div>
+      </div>
+      <br />
+      <br />
+      <div className="main_content">
+        <table className="table">
+          <tr>
+            <th>Product Id</th>
+            <th>Product Name</th>
+            <th>Price</th>
+            <th>Action</th>
+          </tr>
+
+          {pendingProduct.map((item) => (
+            <tr>
+              <td>{item.Product.ProductId}</td>
+              <td>{item.Product.Name}</td>
+              <td>{item.Product.Address}</td>
+              <td>{item.Product.Price}</td>
+              <th>
+                <a className="btn btn-sm btn-primary">Delivered</a>
+              </th>
+            </tr>
+          ))}
+        </table>
       </div>
     </React.Fragment>
   );
