5cad8afc079dbedc79a52f12ad2ee4360251801b
Syed Abdullah <syed_khoab@outlook.com>
2022-01-16 16:30:45 +0600

    customer order completed only push to db left

--- e-hut/src/components/CustomerOrderDashboard/orderDashBoard.js
+++ e-hut/src/components/CustomerOrderDashboard/orderDashBoard.js
@@ -1,26 +1,63 @@
-import React from "react";
+import React, { useState } from "react";
 import "./orderDashBoard.css";
+import PlacedOrder from "./PlacedOrders/placedOrder";
+import OrderHistory from "./OrderHistory/orderHistory";
+import Rating from "./ProvideRating/ProvideRating";
 
-const orderDashboard = () => {
+const OrderDashboard = () => {
+	const [showOrder, setShowOrder] = useState(true);
+	const [showHistory, setShowHistory] = useState(false);
+	const [showProvideRating, setShowProvideRating] = useState(false);
+	const [itemData, setItemData] = useState([]);
+
+	let PlacedOrderClick = () => {
+		setShowOrder(true);
+		setShowHistory(false);
+		setShowProvideRating(false);
+	};
+	let OrderHistoryClick = () => {
+		setShowOrder(false);
+		setShowHistory(true);
+		setShowProvideRating(false);
+	};
+
+	let ProvideRatingHandler = (e) => {
+		setItemData(e.currentTarget.getAttribute("itemValue"));
+		//console.log(e.currentTarget.getAttribute("itemValue"));
+		setShowOrder(false);
+		setShowHistory(false);
+		setShowProvideRating(true);
+	};
+
 	return (
 		<React.Fragment>
 			<div className="dashBoard">
 				<table>
 					<tr>
 						<td>
-							<a className="btn btn-primary btn-sm">Current Orders</a>
+							<button
+								className="btn btn-primary btn-sm"
+								onClick={PlacedOrderClick}
+							>
+								Placed Orders
+							</button>
 						</td>
 						<td>
-							<a className="btn btn-primary btn-sm">Give Rating</a>
-						</td>
-						<td>
-							<a className="btn btn-primary btn-sm">Order History</a>
+							<button
+								className="btn btn-primary btn-sm"
+								onClick={OrderHistoryClick}
+							>
+								Order History
+							</button>
 						</td>
 					</tr>
 				</table>
+				{showOrder && <PlacedOrder />}
+				{showHistory && <OrderHistory handlerFunc={ProvideRatingHandler} />}
+				{showProvideRating && <Rating id={itemData} />}
 			</div>
 		</React.Fragment>
 	);
 };
 
-export default orderDashboard;
+export default OrderDashboard;
