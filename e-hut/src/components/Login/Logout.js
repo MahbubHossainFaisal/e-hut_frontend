9b06076df8df8f9f39eab71811fe87d400b66ecc
Syed Abdullah <syed_khoab@outlook.com>
2022-01-18 21:39:59 +0600

    logout fixed

--- e-hut/src/components/Login/Logout.js
+++ e-hut/src/components/Login/Logout.js
@@ -1,12 +1,19 @@
-import React,{useContext} from "react";
+import React, { useContext } from "react";
+import { useHistory } from "react-router-dom";
 import LoginContext from "../store/loginStatus-context";
 
 const LogOut = () => {
-  const loginCtx = useContext(LoginContext)
-  loginCtx.changeLogin(false)
-  localStorage.clear();
-  //window.location.reload(false);
-  return <React.Fragment></React.Fragment>;
+	const history = useHistory();
+	const loginCtx = useContext(LoginContext);
+	loginCtx.changeLogin(false);
+	localStorage.clear();
+	history.push({
+		pathname: "/home",
+		state: true,
+	});
+	alert("User logged out");
+	//window.location.reload(false);
+	return <React.Fragment></React.Fragment>;
 };
 
 export default LogOut;
