79790eb8a282156ab1b8f153892d3f642d4cc9af
Syed Abdullah <syed_khoab@outlook.com>
2022-02-23 11:20:03 +0600

    Phone number unique validation

--- e-hut/src/components/AdminDashboard/Admin/RegisterAdmin.js
+++ e-hut/src/components/AdminDashboard/Admin/RegisterAdmin.js
@@ -5,212 +5,229 @@ import Dashboard from "../dashboard";
 import { useEffect } from "react";
 
 const RegisterAdmin = () => {
-  const [name, setName] = useState("");
-  const [phone, setPhone] = useState("");
-  const [email, setEmail] = useState("");
-  const [gender, setGender] = useState("");
-  const [password, setPassword] = useState("");
-  const [confirmPassword, setConfirmPassword] = useState("");
-  const [address, setAddress] = useState("");
-  var data = JSON.parse(localStorage.getItem("user"));
-  var cred = data.Phone + ":" + data.Password;
+	const [name, setName] = useState("");
+	const [phone, setPhone] = useState("");
+	const [email, setEmail] = useState("");
+	const [gender, setGender] = useState("");
+	const [password, setPassword] = useState("");
+	const [confirmPassword, setConfirmPassword] = useState("");
+	const [address, setAddress] = useState("");
+	var data = JSON.parse(localStorage.getItem("user"));
+	var cred = data.Phone + ":" + data.Password;
 
-  const [formErrors, setFormErrors] = useState({});
-  const [isSubmit, setIsSubmit] = useState(false);
+	const [formErrors, setFormErrors] = useState({});
+	const [isSubmit, setIsSubmit] = useState(false);
+	const [numExist, setNumExist] = useState("");
 
-  const SubmitHandler = async (e) => {
-    e.preventDefault();
-    setIsSubmit(true);
-    setFormErrors(Validate());
-    e.preventDefault();
-  };
+	const SubmitHandler = async (e) => {
+		e.preventDefault();
+		setIsSubmit(true);
+		setFormErrors(Validate());
+		e.preventDefault();
+	};
 
-  const Validate = () => {
-    const errors = {};
+	useEffect(() => {
+		axios
+			.get("https://localhost:44390/api/Customers/GetExisting/" + phone + "")
+			.then((response) => {
+				if (response.data == "new") {
+					setNumExist("");
+				} else {
+					setNumExist("Number Already Exists.");
+				}
+			})
+			.catch((err) => {
+				console.log(err);
+			});
+	}, [phone]);
 
-    if (name === "") {
-      errors.name = " Name Is Requeired";
-    }
-    if (phone === "") {
-      errors.phone = "Phone Is Requeired";
-    }
-    if (email === "") {
-      errors.email = " Email Is Requeired";
-    }
-    if (gender === "") {
-      errors.gender = "Gender Is Requeired";
-    }
-    if (password === "") {
-      errors.password = "Password Is Requeired";
-    }
-    if (confirmPassword === "") {
-      errors.confirmPassword = "Confirm Password Is Requeired";
-    } else if (password != confirmPassword) {
-      errors.confirmPassword = "Password don't match";
-    }
+	const Validate = () => {
+		const errors = {};
 
-    if (password === "" && confirmPassword != "") {
-      errors.password = "Password Is Requeired";
-      errors.confirmPassword = "";
-    }
+		if (name === "") {
+			errors.name = " Name Is Requeired";
+		}
+		if (phone === "") {
+			errors.phone = "Phone Is Requeired";
+		}
+		if (email === "") {
+			errors.email = " Email Is Requeired";
+		}
+		if (gender === "") {
+			errors.gender = "Gender Is Requeired";
+		}
+		if (password === "") {
+			errors.password = "Password Is Requeired";
+		}
+		if (confirmPassword === "") {
+			errors.confirmPassword = "Confirm Password Is Requeired";
+		} else if (password != confirmPassword) {
+			errors.confirmPassword = "Password don't match";
+		}
 
-    if (address === "") {
-      errors.address = "Address Is Requeired";
-    }
-    //console.log(errors);
-    console.log(formErrors);
-    return errors;
-  };
+		if (password === "" && confirmPassword != "") {
+			errors.password = "Password Is Requeired";
+			errors.confirmPassword = "";
+		}
 
-  useEffect(() => {
-    if (Object.keys(formErrors).length === 0 && isSubmit) {
-      console.log("calling ready for api");
-      axios
-        .post(
-          "https://localhost:44390/api/Admins",
-          {
-            Name: name,
-            Address: address,
-            Phone: phone,
-            Email: email,
-            Gender: gender,
-            Password: password,
-          },
-          {
-            headers: {
-              Authorization: "Basic " + btoa(cred),
-            },
-          }
-        )
-        .then((response) => {
-          alert("Admin Created");
-          setName("");
-          setPhone("");
-          setEmail("");
-          setGender("");
-          setPassword("");
-          setConfirmPassword("");
-          setAddress("");
-        })
-        .catch((err) => {
-          console.log(err);
-        });
-    }
-  }, [formErrors]);
+		if (address === "") {
+			errors.address = "Address Is Requeired";
+		}
+		//console.log(errors);
+		console.log(formErrors);
+		return errors;
+	};
 
-  return (
-    <React.Fragment>
-      <Dashboard />
-      <br />
-      <br />
-      <br />
-      <form action="" onSubmit={SubmitHandler}>
-        <div className="form-group form-content">
-          <input
-            className="form-control"
-            type="text"
-            placeholder="Enter your Name"
-            value={name}
-            onChange={(e) => setName(e.target.value)}
-          />
-          <p className="error">{formErrors.name}</p>
-          <br />
-          <input
-            className="form-control"
-            type="number"
-            placeholder="Enter Your Phone Number"
-            value={phone}
-            onChange={(e) => setPhone(e.target.value)}
-          />
-          <p className="error">{formErrors.phone}</p>
-          <br />
-          <input
-            className="form-control"
-            type="email"
-            placeholder="Enter Your Email Address"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-          />
-          <p className="error">{formErrors.email}</p>
-          <br />
-          <div className="gender-radio ">
-            <label className="text-md-start">Select Gender</label>
-            <br />
-            <div className="row">
-              <div className="col">
-                <input
-                  className="form-check-input "
-                  type="radio"
-                  name="gender"
-                  value="Male"
-                  id=""
-                  onChange={(e) => setGender(e.target.value)}
-                />
-                <label htmlFor="">Male</label>
-              </div>
-              <div className="col">
-                <input
-                  className="form-check-input"
-                  type="radio"
-                  name="gender"
-                  value="Female"
-                  id=""
-                  onChange={(e) => setGender(e.target.value)}
-                />
-                <label htmlFor="">Female</label>
-              </div>
-              <div className="col"></div>
-              <div className="col"></div>
-              <div className="col"></div>
-            </div>
-          </div>
-          <p className="error">{formErrors.gender}</p>
-          <br />
-          <input
-            className="form-control"
-            type="password"
-            name="password"
-            placeholder="Enter your password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-          />
-          <p className="error">{formErrors.password}</p>
-          <br />
-          <input
-            className="form-control"
-            type="password"
-            name="password"
-            placeholder="Confirm your password"
-            value={confirmPassword}
-            onChange={(e) => setConfirmPassword(e.target.value)}
-          />{" "}
-          <p className="error">{formErrors.confirmPassword}</p>
-          <br />
-          <input
-            className="form-control"
-            type="text"
-            placeholder="Enter Your Address"
-            value={address}
-            onChange={(e) => setAddress(e.target.value)}
-          />
-          <p className="error">{formErrors.address}</p>
-          <br />
-          <div className="row">
-            <button
-              type="submit"
-              className="btnSubmit  btn-primary btn-sm text-center"
-            >
-              Register Admin
-            </button>
-          </div>
-        </div>
-      </form>
-      <br />
-      <br />
-      <br />
-      <br />
-    </React.Fragment>
-  );
+	useEffect(() => {
+		if (Object.keys(formErrors).length === 0 && isSubmit) {
+			console.log("calling ready for api");
+			axios
+				.post(
+					"https://localhost:44390/api/Admins",
+					{
+						Name: name,
+						Address: address,
+						Phone: phone,
+						Email: email,
+						Gender: gender,
+						Password: password,
+					},
+					{
+						headers: {
+							Authorization: "Basic " + btoa(cred),
+						},
+					}
+				)
+				.then((response) => {
+					alert("Admin Created");
+					setName("");
+					setPhone("");
+					setEmail("");
+					setGender("");
+					setPassword("");
+					setConfirmPassword("");
+					setAddress("");
+				})
+				.catch((err) => {
+					console.log(err);
+				});
+		}
+	}, [formErrors]);
+
+	return (
+		<React.Fragment>
+			<Dashboard />
+			<br />
+			<br />
+			<br />
+			<form action="" onSubmit={SubmitHandler}>
+				<div className="form-group form-content">
+					<input
+						className="form-control"
+						type="text"
+						placeholder="Enter your Name"
+						value={name}
+						onChange={(e) => setName(e.target.value)}
+					/>
+					<p className="error">{formErrors.name}</p>
+					<br />
+					<input
+						className="form-control"
+						type="number"
+						placeholder="Enter Your Phone Number"
+						value={phone}
+						onChange={(e) => setPhone(e.target.value)}
+					/>
+					<p className="error">{formErrors.phone}</p>
+					<p className="error">{numExist}</p>
+					<br />
+					<input
+						className="form-control"
+						type="email"
+						placeholder="Enter Your Email Address"
+						value={email}
+						onChange={(e) => setEmail(e.target.value)}
+					/>
+					<p className="error">{formErrors.email}</p>
+					<br />
+					<div className="gender-radio ">
+						<label className="text-md-start">Select Gender</label>
+						<br />
+						<div className="row">
+							<div className="col">
+								<input
+									className="form-check-input "
+									type="radio"
+									name="gender"
+									value="Male"
+									id=""
+									onChange={(e) => setGender(e.target.value)}
+								/>
+								<label htmlFor="">Male</label>
+							</div>
+							<div className="col">
+								<input
+									className="form-check-input"
+									type="radio"
+									name="gender"
+									value="Female"
+									id=""
+									onChange={(e) => setGender(e.target.value)}
+								/>
+								<label htmlFor="">Female</label>
+							</div>
+							<div className="col"></div>
+							<div className="col"></div>
+							<div className="col"></div>
+						</div>
+					</div>
+					<p className="error">{formErrors.gender}</p>
+					<br />
+					<input
+						className="form-control"
+						type="password"
+						name="password"
+						placeholder="Enter your password"
+						value={password}
+						onChange={(e) => setPassword(e.target.value)}
+					/>
+					<p className="error">{formErrors.password}</p>
+					<br />
+					<input
+						className="form-control"
+						type="password"
+						name="password"
+						placeholder="Confirm your password"
+						value={confirmPassword}
+						onChange={(e) => setConfirmPassword(e.target.value)}
+					/>{" "}
+					<p className="error">{formErrors.confirmPassword}</p>
+					<br />
+					<input
+						className="form-control"
+						type="text"
+						placeholder="Enter Your Address"
+						value={address}
+						onChange={(e) => setAddress(e.target.value)}
+					/>
+					<p className="error">{formErrors.address}</p>
+					<br />
+					<div className="row">
+						<button
+							type="submit"
+							className="btnSubmit  btn-primary btn-sm text-center"
+						>
+							Register Admin
+						</button>
+					</div>
+				</div>
+			</form>
+			<br />
+			<br />
+			<br />
+			<br />
+		</React.Fragment>
+	);
 };
 
 export default RegisterAdmin;
--- e-hut/src/components/SignUp/SignUp.js
+++ e-hut/src/components/SignUp/SignUp.js
@@ -5,228 +5,232 @@ import "./SignUp.css";
 import { useEffect } from "react";
 
 const SignUp = () => {
-  const [name, setName] = useState("");
-  const [phone, setPhone] = useState("");
-  const [email, setEmail] = useState("");
-  const [gender, setGender] = useState("");
-  const [password, setPassword] = useState("");
-  const [confirmPassword, setConfirmPassword] = useState("");
-  const [address, setAddress] = useState("");
+	const [name, setName] = useState("");
+	const [phone, setPhone] = useState("");
+	const [email, setEmail] = useState("");
+	const [gender, setGender] = useState("");
+	const [password, setPassword] = useState("");
+	const [confirmPassword, setConfirmPassword] = useState("");
+	const [address, setAddress] = useState("");
 
-  const [formErrors, setFormErrors] = useState({});
-  const [isSubmit, setIsSubmit] = useState(false);
+	const [formErrors, setFormErrors] = useState({});
+	const [isSubmit, setIsSubmit] = useState(false);
 
-  const SubmitHandler = async (e) => {
-    e.preventDefault();
-    setIsSubmit(true);
-    setFormErrors(Validate());
-    e.preventDefault();
-  };
+	const [numExist, setNumExist] = useState("");
 
-  const handleBlur = (e) => {
-    setPhone(e.target.value);
-    axios
-      .get("https://localhost:44390/api/Customers/GetExisting/" + phone + "")
-      .then((response) => {
-        if (response.data == "new") {
-          console.warn(response.data);
-          console.warn(phone);
-        } else {
-          console.warn(response.data);
-          console.warn(phone);
-        }
-      })
-      .catch((err) => {
-        console.log(err);
-      });
-  };
+	const SubmitHandler = async (e) => {
+		e.preventDefault();
+		setIsSubmit(true);
+		setFormErrors(Validate());
+		e.preventDefault();
+	};
 
-  const Validate = () => {
-    const errors = {};
+	const handleBlur = (e) => {
+		setPhone(e.target.value);
+	};
 
-    if (name === "") {
-      errors.name = " Name Is Requeired";
-    }
-    if (phone === "") {
-      errors.phone = "Phone Is Requeired";
-    }
-    if (email === "") {
-      errors.email = " Email Is Requeired";
-    }
-    if (gender === "") {
-      errors.gender = "Gender Is Requeired";
-    }
-    if (password === "") {
-      errors.password = "Password Is Requeired";
-    }
-    if (confirmPassword === "") {
-      errors.confirmPassword = "Confirm Password Is Requeired";
-    } else if (password != confirmPassword) {
-      errors.confirmPassword = "Password don't match";
-    }
+	useEffect(() => {
+		axios
+			.get("https://localhost:44390/api/Customers/GetExisting/" + phone + "")
+			.then((response) => {
+				if (response.data == "new") {
+					setNumExist("");
+				} else {
+					setNumExist("Number Already Exists.");
+				}
+			})
+			.catch((err) => {
+				console.log(err);
+			});
+	}, [phone]);
 
-    if (password === "" && confirmPassword != "") {
-      errors.password = "Password Is Requeired";
-      errors.confirmPassword = "";
-    }
+	const Validate = () => {
+		const errors = {};
 
-    if (address === "") {
-      errors.address = "Address Is Requeired";
-    }
-    //console.log(errors);
-    console.log(formErrors);
-    return errors;
-  };
+		if (name === "") {
+			errors.name = " Name Is Requeired";
+		}
+		if (phone === "") {
+			errors.phone = "Phone Is Requeired";
+		}
+		if (email === "") {
+			errors.email = " Email Is Requeired";
+		}
+		if (gender === "") {
+			errors.gender = "Gender Is Requeired";
+		}
+		if (password === "") {
+			errors.password = "Password Is Requeired";
+		}
+		if (confirmPassword === "") {
+			errors.confirmPassword = "Confirm Password Is Requeired";
+		} else if (password != confirmPassword) {
+			errors.confirmPassword = "Password don't match";
+		}
 
-  useEffect(() => {
-    if (Object.keys(formErrors).length === 0 && isSubmit) {
-      console.log("calling ready for api");
-      axios
-        .post("https://localhost:44390/api/Customers", {
-          Name: name,
-          Address: address,
-          Phone: phone,
-          Email: email,
-          Gender: gender,
-          Password: password,
-        })
-        .then((response) => {
-          alert("Customer Created");
-          setName("");
-          setPhone("");
-          setEmail("");
-          setGender("");
-          setPassword("");
-          setConfirmPassword("");
-          setAddress("");
-        })
-        .catch((err) => {
-          console.log(err);
-        });
-    }
-  }, [formErrors]);
+		if (password === "" && confirmPassword != "") {
+			errors.password = "Password Is Requeired";
+			errors.confirmPassword = "";
+		}
 
-  return (
-    <React.Fragment>
-      <br />
-      <br />
-      <br />
-      <form action="" onSubmit={SubmitHandler}>
-        <div className="form-group form-content">
-          <input
-            className="form-control"
-            type="text"
-            placeholder="Enter your Name"
-            value={name}
-            onChange={(e) => setName(e.target.value)}
-          />
-          <p className="error">{formErrors.name}</p>
-          <br />
-          <input
-            className="form-control"
-            type="number"
-            placeholder="Enter Your Phone Number"
-            onBlur={handleBlur}
-          />
-          <p className="error">{formErrors.phone}</p>
-          <br />
-          <input
-            className="form-control"
-            type="email"
-            placeholder="Enter Your Email Address"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-          />
-          <p className="error">{formErrors.email}</p>
-          <br />
-          <div className="gender-radio ">
-            <label className="text-md-start">Select Gender</label>
-            <br />
-            <div className="row">
-              <div className="col">
-                <input
-                  className="form-check-input "
-                  type="radio"
-                  name="gender"
-                  value="Male"
-                  id=""
-                  onChange={(e) => setGender(e.target.value)}
-                />
-                <label htmlFor="">Male</label>
-              </div>
-              <div className="col">
-                <input
-                  className="form-check-input"
-                  type="radio"
-                  name="gender"
-                  value="Female"
-                  id=""
-                  onChange={(e) => setGender(e.target.value)}
-                />
-                <label htmlFor="">Female</label>
-              </div>
-              <div className="col"></div>
-              <div className="col"></div>
-              <div className="col"></div>
-            </div>
-          </div>
-          <p className="error">{formErrors.gender}</p>
-          <br />
-          <input
-            className="form-control"
-            type="password"
-            name="password"
-            placeholder="Enter your password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-          />
-          <p className="error">{formErrors.password}</p>
-          <br />
-          <input
-            className="form-control"
-            type="password"
-            name="password"
-            placeholder="Confirm your password"
-            value={confirmPassword}
-            onChange={(e) => setConfirmPassword(e.target.value)}
-          />{" "}
-          <p className="error">{formErrors.confirmPassword}</p>
-          <br />
-          <input
-            className="form-control"
-            type="text"
-            placeholder="Enter Your Address"
-            value={address}
-            onChange={(e) => setAddress(e.target.value)}
-          />
-          <p className="error">{formErrors.address}</p>
-          <br />
-          <div className="row">
-            <button
-              type="submit"
-              className="btnSubmit  btn-primary btn-sm text-center"
-            >
-              SignUp
-            </button>
-          </div>
-          <div className="row">
-            <Link to="/signup/shop">
-              <button
-                type="button"
-                className="btnSubmit  btn-primary btn-sm text-center my-2"
-              >
-                SignUp For Your Shop
-              </button>
-            </Link>
-          </div>
-        </div>
-      </form>
-      <br />
-      <br />
-      <br />
-      <br />
-    </React.Fragment>
-  );
+		if (address === "") {
+			errors.address = "Address Is Requeired";
+		}
+		//console.log(errors);
+		console.log(formErrors);
+		return errors;
+	};
+
+	useEffect(() => {
+		if (Object.keys(formErrors).length === 0 && isSubmit) {
+			console.log("calling ready for api");
+			axios
+				.post("https://localhost:44390/api/Customers", {
+					Name: name,
+					Address: address,
+					Phone: phone,
+					Email: email,
+					Gender: gender,
+					Password: password,
+				})
+				.then((response) => {
+					alert("Customer Created");
+					setName("");
+					setPhone("");
+					setEmail("");
+					setGender("");
+					setPassword("");
+					setConfirmPassword("");
+					setAddress("");
+				})
+				.catch((err) => {
+					console.log(err);
+				});
+		}
+	}, [formErrors]);
+
+	return (
+		<React.Fragment>
+			<br />
+			<br />
+			<br />
+			<form action="" onSubmit={SubmitHandler}>
+				<div className="form-group form-content">
+					<input
+						className="form-control"
+						type="text"
+						placeholder="Enter your Name"
+						value={name}
+						onChange={(e) => setName(e.target.value)}
+					/>
+					<p className="error">{formErrors.name}</p>
+					<br />
+					<input
+						className="form-control"
+						type="text"
+						placeholder="Enter Your Phone Number"
+						onChange={handleBlur}
+					/>
+					<p className="error">{formErrors.phone}</p>
+					<p className="error">{numExist}</p>
+					<br />
+					<input
+						className="form-control"
+						type="email"
+						placeholder="Enter Your Email Address"
+						value={email}
+						onChange={(e) => setEmail(e.target.value)}
+					/>
+					<p className="error">{formErrors.email}</p>
+					<br />
+					<div className="gender-radio ">
+						<label className="text-md-start">Select Gender</label>
+						<br />
+						<div className="row">
+							<div className="col">
+								<input
+									className="form-check-input "
+									type="radio"
+									name="gender"
+									value="Male"
+									id=""
+									onChange={(e) => setGender(e.target.value)}
+								/>
+								<label htmlFor="">Male</label>
+							</div>
+							<div className="col">
+								<input
+									className="form-check-input"
+									type="radio"
+									name="gender"
+									value="Female"
+									id=""
+									onChange={(e) => setGender(e.target.value)}
+								/>
+								<label htmlFor="">Female</label>
+							</div>
+							<div className="col"></div>
+							<div className="col"></div>
+							<div className="col"></div>
+						</div>
+					</div>
+					<p className="error">{formErrors.gender}</p>
+					<br />
+					<input
+						className="form-control"
+						type="password"
+						name="password"
+						placeholder="Enter your password"
+						value={password}
+						onChange={(e) => setPassword(e.target.value)}
+					/>
+					<p className="error">{formErrors.password}</p>
+					<br />
+					<input
+						className="form-control"
+						type="password"
+						name="password"
+						placeholder="Confirm your password"
+						value={confirmPassword}
+						onChange={(e) => setConfirmPassword(e.target.value)}
+					/>{" "}
+					<p className="error">{formErrors.confirmPassword}</p>
+					<br />
+					<input
+						className="form-control"
+						type="text"
+						placeholder="Enter Your Address"
+						value={address}
+						onChange={(e) => setAddress(e.target.value)}
+					/>
+					<p className="error">{formErrors.address}</p>
+					<br />
+					<div className="row">
+						<button
+							type="submit"
+							className="btnSubmit  btn-primary btn-sm text-center"
+						>
+							SignUp
+						</button>
+					</div>
+					<div className="row">
+						<Link to="/signup/shop">
+							<button
+								type="button"
+								className="btnSubmit  btn-primary btn-sm text-center my-2"
+							>
+								SignUp For Your Shop
+							</button>
+						</Link>
+					</div>
+				</div>
+			</form>
+			<br />
+			<br />
+			<br />
+			<br />
+		</React.Fragment>
+	);
 };
 
 export default SignUp;
